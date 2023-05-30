59267bacc87c7a1804b0a70e6c0ad516b2ecbfd6
emrekavurur <emre.kavurur@student-htw.berlin.de>
2023-05-30 14:04:54 +0200

    init

---
+++ src/main/java/de/htwberlin/webtechnologien/Geburtstagseinkaufliste/GeburtstagseinkauflisteApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechnologien.Geburtstagseinkaufliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeburtstagseinkauflisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeburtstagseinkauflisteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtechnologien/Geburtstagseinkaufliste/GeburtstagseinkauflisteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtechnologien.Geburtstagseinkaufliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeburtstagseinkauflisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
